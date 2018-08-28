1db0c833e8c76942eefced9d2b54bd9e73f12984
Brett Kinnamon <bdkinna@gmail.com>
2018-08-28 07:01:18 -0400

    Add a simple dialog

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react'
 
 import './App.css'
 
-import MonthsSelect from './controls/MonthsSelect'
+import Dialog from './components/Dialog'
 
 export class App extends Component {
   render() {
     return (
       <div>
-        <MonthsSelect onChange={value => console.dir(value)} />
+        <Dialog />
       </div>
     )
   }
---
+++ src/components/Dialog.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+import './css/Dialog.css'
+
+export class Dialog extends Component {
+  render() {
+    return(
+      <div className='dialog-wrapper'>
+        <div className='dialog'>
+          Hello world!
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Dialog
